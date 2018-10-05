d24ef8d16be25fd0bdccc0c744eed373002875b8
kileyr <kileylroberson@gmail.com>
2018-10-05 10:44:56 -0700

    Multiplication

---
+++ src/Multiplication.java
@@ -0,0 +1,11 @@
+
+public class Multiplication {
+
+	public static Integer mult(Integer x, Integer y) {
+		return x * y;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(mult(4,4));
+	}
+}
